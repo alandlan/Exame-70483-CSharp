595cb2e2b46fafbdaea31d5640d3d6f80bcdc3bf
Jéssica Nathany Carvalho Freitas <jnathany@outlook.com>
2019-10-10 08:34:38 -0300

    criação da classe modificadores de acesso

---
+++ Exame-70483_CSharp/Tipos/ModificadoresAcesso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos
+{
+    /// <summary>
+    /// Modificadores de acesso do C#
+    /// </summary>
+    public class ModificadoresAcesso
+    {
+        /// <summary>
+        /// Modificadore public o acesso não é restrito
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// É um modificador de acesso restrito, atributos e métodos 
+        /// declarados com protected, são acessíveis somente pela classe declarada e suas subclasses
+        /// </summary>
+        protected string NomeProtegido {get; set;}
+    }
+}

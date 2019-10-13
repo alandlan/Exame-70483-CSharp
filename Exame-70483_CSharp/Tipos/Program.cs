50fda2ac6e62073942065c5228bf185148c49b01
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-10-13 20:40:27 -0300

    Criado Enum

--- Exame-70483_CSharp/Tipos/Program.cs
+++ Exame-70483_CSharp/Tipos/Program.cs
@@ -10,7 +10,17 @@ namespace Tipos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            TiposValor tiposValor = new TiposValor(1, 1, '1', 1.0M, 200, 25, 100, 32, 345, 23465, 1324455, 2);
+
+            Console.WriteLine(tiposValor.Percentual);
+
+            int DiaDaSemana = Convert.ToInt32(DiasDaSemana.Dom);
+
+            Console.WriteLine(DiaDaSemana);
+
+
+            Console.ReadKey();
+
         }
     }
 }

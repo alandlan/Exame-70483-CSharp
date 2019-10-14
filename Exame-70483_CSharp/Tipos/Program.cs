b22fca70af27933456d666e76c6691d3e9746251
Jéssica Nathany Carvalho Freitas <jnathany@outlook.com>
2019-10-13 21:34:07 -0300

    declarando os tipos c#

--- Exame-70483_CSharp/Tipos/Program.cs
+++ Exame-70483_CSharp/Tipos/Program.cs
@@ -12,8 +12,59 @@ namespace Tipos
         {
             // Tipos C#
             int numero = 100;
-            string nome = "Teste";
-            decimal valor = 360M;
+            string nome = "Teste AB";
+            float valor = 12.5F;
+            decimal valorTotal = 360M;
+            bool ativo = false;
+            byte numeroQuarto = 108;
+            sbyte grauCelsius = -2;
+            short quantidade = 3690;
+            ushort quantidade2 = 6744;
+            uint QtdeRegistros = 43905;
+            long numerosLongo = -38374;
+            long numerosLongo2 = -38374947;
+            char sexo = 'F';
+
+            Console.WriteLine("Estes são os tipos do C#");
+
+            Console.WriteLine("Tipo int: {0}", numero);
+
+            Console.WriteLine("Tipo string: {0}", nome);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo decimal: {0}", valorTotal);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo float: {0}", valor);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo bool: {0}", ativo);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo sbyte: {0}", grauCelsius);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo byte: {0}", numeroQuarto);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo short: {0}", quantidade);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo ushort: {0}", quantidade2);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo uint: {0}", QtdeRegistros);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo long: {0}", numerosLongo);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo ulong: {0}", numerosLongo2);
+            Console.ReadLine();
+
+            Console.WriteLine("Tipo char: {0}", sexo);
+            Console.ReadLine();
+
         }
     }
 }

af574cd6fb7914329fa58d2b50a09830bb8a4828
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-10-15 08:19:52 -0300

    Propriedades do Banco

--- Exame-70483_CSharp/Tipos/Banco.cs
+++ Exame-70483_CSharp/Tipos/Banco.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Tipos
 {
-   public class Banco
+    public class Banco
     {
+        /// <summary>
+        /// Identificador unico da conta
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Numero da Conta
+        /// </summary>
+        public string NomeBanco { get; set; }
+
+        /// <summary>
+        /// Numero da conta
+        /// </summary>
+        public int NumeroConta { get; set; }
+
+        /// <summary>
+        /// Numero do digito
+        /// </summary>
+        public short NumeroDigito { get; set; }
+
+        /// <summary>
+        /// Numero da agencia
+        /// </summary>
+        public short NumeroAgencia { get; set; }
     }
 }

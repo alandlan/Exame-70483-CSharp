6d1db6f9584c1c02265dff98d9954336f5f74b34
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-10-15 08:26:05 -0300

    Adicionado propriedades TipoConta

---
+++ Exame-70483_CSharp/Tipos/TipoConta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos
+{
+    public class TipoConta
+    {
+        /// <summary>
+        /// Id da Conta
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome da Conta
+        /// </summary>
+        public string NomeConta { get; set; }
+    }
+}
